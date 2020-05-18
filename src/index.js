0114d651a26c6a2fdb9ae36dae34bfeaa2eec143
vahidsediqi <vsediqi@live.com>
2020-05-18 18:45:00 +0300

    scrollToTo added again

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Router  } from 'react-router-dom'
 import './index.css';
+import ScrollToTop from './components/ScrollToTop';
 import App from './App';
 
 
@@ -9,6 +10,7 @@ ReactDOM.render(
   <React.StrictMode>
      <HashRouter>
               <App />
+              <ScrollToTop />
     </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
