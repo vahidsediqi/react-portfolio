b8930db669891d88ce836833033ad43c989465b8
vahidsediqi <vsediqi@live.com>
2020-05-18 13:49:14 +0300

    Updates

---
+++ precache-manifest.f34fc6cf5918db7932193b05b39b4402.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8de3a629d84cca977b4f76f3709c2a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37933a7a8ca1dd684616",
+    "url": "/static/css/2.ef52ff12.chunk.css"
+  },
+  {
+    "revision": "8649be626597af444a55",
+    "url": "/static/css/main.c5969835.chunk.css"
+  },
+  {
+    "revision": "37933a7a8ca1dd684616",
+    "url": "/static/js/2.eecd5f56.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/static/js/2.eecd5f56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8649be626597af444a55",
+    "url": "/static/js/main.20849d3a.chunk.js"
+  },
+  {
+    "revision": "8029a1c8c7856952aa99",
+    "url": "/static/js/runtime-main.e861f681.js"
+  },
+  {
+    "revision": "f5de3e3e5c87c4f93eaa626ce64dd911",
+    "url": "/static/media/about-slider.f5de3e3e.jpg"
+  },
+  {
+    "revision": "bf5bc969e9abe27bc90d024d0d935eaa",
+    "url": "/static/media/about.bf5bc969.jpg"
+  },
+  {
+    "revision": "122b15d0d3e067da05d502411b586940",
+    "url": "/static/media/bookapp.122b15d0.png"
+  },
+  {
+    "revision": "3c7181135ed585c646b1810cdbc6bd39",
+    "url": "/static/media/calc.3c718113.png"
+  },
+  {
+    "revision": "d6dc5e6fcbe8c6c77c142dc8520293cf",
+    "url": "/static/media/cms.d6dc5e6f.jpg"
+  },
+  {
+    "revision": "f7d7fc6a0331884c4fa485a3627908cd",
+    "url": "/static/media/disgners.f7d7fc6a.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4d14af1acf9914d2c3b6d55dc3db26b4",
+    "url": "/static/media/gym.4d14af1a.png"
+  },
+  {
+    "revision": "39ca5e3817a286e8b7858fe3ae88d280",
+    "url": "/static/media/h5.39ca5e38.png"
+  },
+  {
+    "revision": "381566afeed6f80e10020208f6c2eabe",
+    "url": "/static/media/mcco.381566af.png"
+  },
+  {
+    "revision": "59e48ab2285f9ac64ea6d45f6be9853b",
+    "url": "/static/media/me.59e48ab2.jpg"
+  },
+  {
+    "revision": "e02b95fdd4da088cc6435ee4eabd7ead",
+    "url": "/static/media/nursing.e02b95fd.png"
+  },
+  {
+    "revision": "0e6aace4cc454584bf97552c3c8a9cf6",
+    "url": "/static/media/patric.0e6aace4.png"
+  },
+  {
+    "revision": "aec4adb561a9b36ff33361bc5bfdbb28",
+    "url": "/static/media/services-bg.aec4adb5.jpg"
+  },
+  {
+    "revision": "5e5046e4c059b4bd803a21ebf772a9eb",
+    "url": "/static/media/slider.5e5046e4.jpg"
+  },
+  {
+    "revision": "8241e8bdaba7e55176b09d92100a2ad2",
+    "url": "/static/media/sui.8241e8bd.png"
+  },
+  {
+    "revision": "6a14b0b6ff61bdc014ca647a52a3978d",
+    "url": "/static/media/webapp.6a14b0b6.jpg"
+  },
+  {
+    "revision": "4e6bee90db7933232cc86e60862e6a97",
+    "url": "/static/media/webdesign.4e6bee90.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.ff93939683719f908a4d534e053b662d.js"
+  "/precache-manifest.f34fc6cf5918db7932193b05b39b4402.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
